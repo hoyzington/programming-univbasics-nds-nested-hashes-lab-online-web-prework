c5a53873b94f2cd6d9d14133ba1476f3272456f0
Scott Hoisington <hoyzington@gmail.com>
2019-11-05 07:00:05 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -12,7 +12,7 @@ def assembled_aoh
 end
 
 def literal_aoh
-  aoh = [{name: "Don Gately", occupation: "Live-in Staff"}, ]
+  aoh = [{name: "Don Gately", occupation: "Live-in Staff"}, { name: "Joelle van Dyne", occupation: "Radio Personality" }, ]
   aoh
 end
 
